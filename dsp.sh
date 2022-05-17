c39fe03e141a28e4d2a3b2779b2b70cf25de9b99
hari2401 <100417576+hari2401@users.noreply.github.com>
2022-05-17 22:26:29 +0530

    k
    k

--- dsp.sh
+++ dsp.sh
@@ -4,4 +4,4 @@ sudo systemctl enable docker
 sudo systemctl start docker
 sudo chmod 666 /var/run/docker.sock
 sudo usermod -aG docker ec2-user 
-#ec2-user or ubuntu which is your using keep change following 
+ 
